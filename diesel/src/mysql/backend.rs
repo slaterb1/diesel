c24df76c98cc8c4f8a2aa9c36a3ddbbce7f22733
Benjamin Slater <benjamin.ed.slater@gmail.com>
2019-10-06 00:31:11 -0700

    added SupportsReturningClause for Mysql

--- diesel/src/mysql/backend.rs
+++ diesel/src/mysql/backend.rs
@@ -78,5 +78,6 @@ impl TypeMetadata for Mysql {
     type MetadataLookup = ();
 }
 
+impl SupportsReturningClause for Mysql {}
 impl SupportsDefaultKeyword for Mysql {}
 impl UsesAnsiSavepointSyntax for Mysql {}
